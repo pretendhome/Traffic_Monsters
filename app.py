e73cebc60e45393507f153b2c685a20802687409
Christian Cayanan <mrccayanan@gmail.com>
2019-08-07 20:43:34 -0700

    Update

--- app.py
+++ app.py
@@ -26,11 +26,19 @@ def welcome():
         </ul> 
         <h2>Plotting Data & Visualization</h2>
         <ul>
+        <li><em>Column Chart & Scatter plot chart Visualizing Top 14 Streets in San Francisco with the Max Rush Hour Traffic</em>
+        </li>
+        </ul>
+        <ul>
         <li>  
-        <a href="https://data.sfgov.org/Transportation/Rush-Hour-Routes/4zr7-yz4w"><em>Click 4 Rush Hour Routes</em><img class="panel" src="https://github.com/ChiAmyC0987/ETL-TRAFFIC-MONSTER/blob/master/Images/Rush%20Hour%20Routes%20Max%20Traffic%20Count.png?raw=true"width=240 alt="Rush Hour Max Column"><img class="panel" src="https://github.com/ChiAmyC0987/ETL-TRAFFIC-MONSTER/blob/master/Images/Rush%20Hour%20Traffic%20Routes%20Scatter%20Plot.png?raw=true"width=240 alt="Rush Hour Max Scatter"></a>
+        <a href="https://data.sfgov.org/Transportation/Rush-Hour-Routes/4zr7-yz4w"><em></em><img class="panel" src="https://github.com/ChiAmyC0987/ETL-TRAFFIC-MONSTER/blob/master/Images/Rush%20Hour%20Routes%20Max%20Traffic%20Count.png?raw=true"width=540 alt="Rush Hour Max Column"><img class="panel" src="https://github.com/ChiAmyC0987/ETL-TRAFFIC-MONSTER/blob/master/Images/Rush%20Hour%20Traffic%20Routes%20Scatter%20Plot.png?raw=true"width=540 alt="Rush Hour Max Scatter"></a>
+        </li>
+        </ul>
+        <ul>
+        <li><em>Bar chart & Pie chart Visualizing Top San Francisco Streets with Maintenance Treatment</em>
         </li>
         <li>
-        <a href="https://data.sfgov.org/City-Infrastructure/Streets-Data-Pavement-Condition-Index-PCI-Scores/5aye-4rtt"><em>Click 4 Street Maintenance</em><img class="panel" src="https://github.com/ChiAmyC0987/ETL-TRAFFIC-MONSTER/blob/master/Images/Streets%20Condition%20with%20Max%20Maintenance%20Bar%20Chart.png?raw=true"width=240 alt="SPC"><img class="panel" src="https://github.com/ChiAmyC0987/ETL-TRAFFIC-MONSTER/blob/master/Images/Streets%20Condition%20with%20Max%20Maintenance%20Pie%20Chart.png?raw=true"width=240 alt="SPC pie chart"></a>
+        <a href="https://data.sfgov.org/City-Infrastructure/Streets-Data-Pavement-Condition-Index-PCI-Scores/5aye-4rtt"><em></em><img class="panel" src="https://github.com/ChiAmyC0987/ETL-TRAFFIC-MONSTER/blob/master/Images/Streets%20Condition%20with%20Max%20Maintenance%20Bar%20Chart.png?raw=true"width=540 alt="SPC"><img class="panel" src="https://github.com/ChiAmyC0987/ETL-TRAFFIC-MONSTER/blob/master/Images/Streets%20Condition%20with%20Max%20Maintenance%20Pie%20Chart.png?raw=true"width=540 alt="SPC pie chart"></a>
         </li>
         </ul>
        <h2> Project Proposal:</h2>
@@ -50,12 +58,15 @@ Take advantage of your Instructor and TA support during office hours and class p
         <p><em>Created a filtered dataframe from specific columns</em></p>
         <p><em>Renamed the column headers</em></p>
         <p><em>Cleaned the data by dropping duplicates and setting the index</em></p>
+        <p><em>Attempted to combine both csv files with the INNER JOINS clause in PostgresSQL</em></p>
         <li><strong>Load:</strong><br>The type of final production database to load the data into (relational or non-relational).         </li>
         <p><em>Created database , table schema , queries with Pgadmin4 & stored in PostgresSQL / Loaded data into PostgresSQL database from Pandas</em></p>
         <li>The final tables or collections that will be used in the production database.</li>
-        <p><em>Loaded DataFrames into PostgresSQL database</em></p>
+        <p><em>Loaded DataFrames into PostgresSQL Database as Traffic_db</em></p>
+            <p><em>Connected to Network with Mongod</em></p><a href="#"><em>Created a Database in <strong>MongoDB Compass</strong> as Traffic_db, Imported the 2 CSV files into 2  Collections</em></a>
+        <h3>Data Analytic Tools used for Project 1</h3>
 
-<p><em> Jupyter , Pandas , PostgresSQL , PgAdmin4 , Flask , Html were used for this project </em></p>
+<p><em> Jupyter , Python, Pandas , PostgresSQL , PgAdmin4 , MongoD, MongoDB Compass, Flask , Html were used for this project </em></p>
 
 
         </body>
