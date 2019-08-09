083c2316ea181fd6063d85e5b689a5fa433a21d0
ChiAmyC <51067413+ChiAmyC0987@users.noreply.github.com>
2019-08-08 20:15:54 -0700

    finally2

---
+++ AADT.sql
@@ -0,0 +1,14 @@
+CREATE TABLE AADT(
+    Year int,
+	District varchar(25),
+	Route int,
+	County varchar(5),
+	Postmile float,
+	Descriptn varchar(50),
+	Back_AADT int,
+	Ahead_AADT int,
+	Lat_S_or_W float,
+	Lon_S_or_W float, 
+	Lat_N_or_E float,
+	Lon_N_or_E float
+);
